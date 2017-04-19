379c19486dbcf40e714d4f6245ba0239186812bd
louyu <746831652@qq.com>
2017-04-19 18:31:53 +0800

    test

---
+++ app/src/main/java/com/ly/materialdesigndemo/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ly.materialdesigndemo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by txw_pc on 2017/4/19.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
