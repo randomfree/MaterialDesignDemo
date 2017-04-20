c4b033124639e70b9c1a59a9158c449d25d92126
louyu <746831652@qq.com>
2017-04-20 18:38:19 +0800

    searchactiviy,目标自动换行recyclerview

--- app/src/main/java/com/ly/materialdesigndemo/activity/MainActivity.java
+++ app/src/main/java/com/ly/materialdesigndemo/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ly.materialdesigndemo.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -127,6 +128,6 @@ public class MainActivity extends BaseActivity {
     }
     @OnClick(R.id.search_rlt)
     public void onViewClicked() {
-
+        startActivity(new Intent(this,SearchActivity.class));
     }
 }
---
+++ app/src/main/java/com/ly/materialdesigndemo/activity/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.ly.materialdesigndemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ly.materialdesigndemo.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
