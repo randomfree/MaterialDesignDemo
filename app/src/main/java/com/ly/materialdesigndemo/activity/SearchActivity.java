a0119924984e1a062ec3e61ac01e5d4b84017e78
louyu <746831652@qq.com>
2017-04-21 17:55:38 +0800

    searchactiviy,自动换行还可以滑动还可以进行回收的recyclerview终于写出来了···· 但是！！！ 如果数据不够recyclerview不能自动改变自己的高 其实这种功能就不应该用recyclerview写 他奶奶的

--- app/src/main/java/com/ly/materialdesigndemo/activity/SearchActivity.java
+++ app/src/main/java/com/ly/materialdesigndemo/activity/SearchActivity.java
@@ -1,15 +1,34 @@
 package com.ly.materialdesigndemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 import com.ly.materialdesigndemo.R;
+import com.ly.materialdesigndemo.adapter.SearchAdapter;
+import com.ly.materialdesigndemo.layoutmanager.AutoNextLineLayoutManger;
+import com.ly.materialdesigndemo.layoutmanager.SpaceItemDecoration;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    @InjectView(R.id.recyclerview_search)
+    RecyclerView recyclerviewSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.inject(this);
+        AutoNextLineLayoutManger autoNextLineLayoutManger = new AutoNextLineLayoutManger();
+        recyclerviewSearch.setLayoutManager(autoNextLineLayoutManger);
+        recyclerviewSearch.setAdapter(new SearchAdapter());
+        recyclerviewSearch.addItemDecoration(new SpaceItemDecoration(20));
     }
 }
