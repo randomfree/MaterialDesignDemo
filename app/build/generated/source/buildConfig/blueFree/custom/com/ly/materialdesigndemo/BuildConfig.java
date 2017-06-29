8105dc5aa10c3c590652d517c826b0a68fa61997
louyu <746831652@qq.com>
2017-06-29 18:44:26 +0800

    gradle study
    gradle study

---
+++ app/build/generated/source/buildConfig/blueFree/custom/com/ly/materialdesigndemo/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ly.materialdesigndemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ly.MaterialDesignDemo.blue.custom";
+  public static final String BUILD_TYPE = "custom";
+  public static final String FLAVOR = "blueFree";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0-custom";
+  public static final String FLAVOR_color = "blue";
+  public static final String FLAVOR_price = "free";
+  // Fields from build type: custom
+  public static final boolean LOG_DEBUG = true;
+}
